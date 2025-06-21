3bcb15bf0e88d24a9df696cc3cfcc3b8431680ef
SAYANDEEP DEY <22052753@kiit.ac.in>
2025-06-21 15:31:29 +0530

    initial commit

---
+++ SingletonPatternExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger1 = Logger.Instance;
+        logger1.Log("Application started.");
+
+        Logger logger2 = Logger.Instance;
+        logger2.Log("Another log message.");
+
+        if (logger1 == logger2)
+        {
+            Console.WriteLine("Both logger instances are the same. Singleton works!");
+        }
+        else
+        {
+            Console.WriteLine("Logger instances are different. Singleton failed!");
+        }
+    }
+}
